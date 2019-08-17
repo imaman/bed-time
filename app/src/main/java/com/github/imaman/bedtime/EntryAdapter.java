b0b691a2ac9de55e72229084d4e03e1906be24d4
Itay Maman <imaman@users.noreply.github.com>
2019-08-17 22:23:26 +0300

    introduce a dedicated custom view for entries

--- app/src/main/java/com/github/imaman/bedtime/EntryAdapter.java
+++ app/src/main/java/com/github/imaman/bedtime/EntryAdapter.java
@@ -17,8 +17,7 @@ class EntryAdapter extends RecyclerView.Adapter<EntryViewHolder> {
     @NonNull
     @Override
     public EntryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.my_text_view, parent, false);
+        View v = new EntryView(parent.getContext());
         return new EntryViewHolder(v);
     }
 
---
+++ app/src/main/java/com/github/imaman/bedtime/EntryView.java
@@ -0,0 +1,17 @@
+package com.github.imaman.bedtime;
+
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
+
+public class EntryView extends LinearLayout {
+    
+    public EntryView(Context context) {
+        super(context);
+
+        LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.entry, this, true);
+
+    }
+}
